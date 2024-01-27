d1e0a58fafd2cdbce5383c83f184ded5d1276472
mugman <53713683+mugman174@users.noreply.github.com>
2024-01-27 00:10:59 -0800

    get latest instead of oldest discord (there is surely a slightly better method)

--- silly.sh
+++ silly.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-filename=$(curl -sL https://ipa.aspy.dev/discord/testflight/ | grep ipa | head -n 1 | cut -f 2 -d '"')
-node index.js "https://ipa.aspy.dev/discord/testflight/${filename:2}"
+filename=$(curl -sL https://ipa.aspy.dev/discord/testflight/ | grep ipa | tail -n 2 | cut -f 2 -d '>' | cut -f 1 -d '<')
+node index.js "https://ipa.aspy.dev/discord/testflight/${filename:1}"
