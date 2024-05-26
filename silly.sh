3affe8b4dd47e7a3675f574b4798a6d994eea2ec
mugman <53713683+mugman174@users.noreply.github.com>
2024-05-25 20:58:25 -0700

    small experiment didn't quite work

--- silly.sh
+++ silly.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 filename=$(curl -sL https://ipa.aspy.dev/discord/testflight/ | grep ipa | tail -n 2 | cut -f 2 -d '>' | cut -f 1 -d '<')
-echo __DISCORD_VERSION=$(echo ${filename:1} | grep '[0-9]*' -oz | awk -F"\0" '{print $1 "_" $3}') >> $GITHUB_OUTPUT
 node index.js "https://ipa.aspy.dev/discord/testflight/${filename:1}"
